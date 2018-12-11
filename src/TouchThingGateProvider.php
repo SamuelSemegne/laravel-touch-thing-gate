667a6be3fce12e3fdabf5f6029a2ecb4593e843b
Sam <samsemegne@gmail.com>
2018-12-11 18:05:28 +0100

    Adds the provider

---
+++ src/TouchThingGateProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Samuelsemegne\Laravel_Touch_Thing_Gate;
+
+use Gate;
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+
+class TouchThingGateProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        /**
+         * This gate checks whether the user and the given model are related.
+         */
+        Gate::define('touch-thing', function (User $user, Model $model) {
+            // Match against the model's user_id property (if it has any).
+            if ($model->user_id) {
+                return ($model->user_id == $user->id);
+            } 
+            // Match against the model belongsTo User relation (if it has any).
+            elseif ($model->user) {
+                return ($model->user->id == $user->id);
+            }
+            // Check if the model IS of type User.
+            elseif (get_class($model) == 'App\User') {
+                return ($model->id == $user->id);
+            }
+            else {
+                return false;
+            }
+        });
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
